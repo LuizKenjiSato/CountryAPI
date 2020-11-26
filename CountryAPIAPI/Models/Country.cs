f652b74318af71d5389f21f4bb45bcef641245df
dchan644 <74440034+dchan644@users.noreply.github.com>
2020-11-26 05:18:00 -0500

    Add files via upload

---
+++ CountryAPIAPI/Models/Country.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CountryAPI.Models
+{
+    public class Country
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string Continent { get; set; }
+        public string CapitalCity { get; set; }
+        public string Population { get; set; }
+        public string Language { get; set; }
+        public string Currency { get; set; }
+        public double CurrencyValue { get; set; }
+    }
+}
---
+++ CountryAPIAPI/Models/CountryContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace CountryAPI.Models
+{
+    public class CountryContext :DbContext
+    {
+        public CountryContext(DbContextOptions<CountryContext> options)
+    : base(options)
+        {
+        }
+
+        public DbSet<Country> Countries { get; set; }
+    }
+}
